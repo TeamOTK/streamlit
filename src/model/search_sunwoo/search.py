0e6a74f5549163e9273956e1e362f0faf6271a1f
hsh <hsh200315@naver.com>
2024-01-22 18:19:15 +0900

    fix: 검색 성능 향상 시도

--- src/model/search_sunwoo/search.py
+++ src/model/search_sunwoo/search.py
@@ -32,7 +32,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY_KSW")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY_HSH")
 # OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 # Output parser will split the LLM result into a list of queries
@@ -60,7 +60,7 @@ class Search():
             csv_args={
             # 'delimiter': ',',
             # 'quotechar': '"',
-            'fieldnames': ['id', 'title', 'genre', 'description']},
+            'fieldnames': ['title', 'author', 'genre', 'description', 'character']},
             encoding = 'UTF-8'
         )
         data = loader.load()
@@ -165,6 +165,7 @@ class Search():
     
     def get_parent_document_retriever(self, embeddings):
         # This text splitter is used to create the child documents
+        parent_splitter = RecursiveCharacterTextSplitter(chunk_size=2000)
         child_splitter = RecursiveCharacterTextSplitter(chunk_size=400)
         # The vectorstore to use to index the child chunks
         vectorstore = Chroma(
@@ -176,13 +177,14 @@ class Search():
             vectorstore=vectorstore,
             docstore=store,
             child_splitter=child_splitter,
+            parent_splitter=parent_splitter,
         )
         return retriever
     
     def add_docs_to_retriever(self, retriever, docs):
         retriever.add_documents(docs, ids=None)
         return retriever
-            
+    
     def get_all_relevant_documents(self, query, retriever):
         # Get relevant documents ordered by relevance score
         docs = retriever.get_relevant_documents(query)
@@ -197,55 +199,6 @@ class Search():
             # result.append(doc.page_content.split("\n")[1])
             result.append(doc.metadata["title"])
         return result
-    
-    def get_sub_relevant_documents(self, query, vectorstore):
-        sub_docs = vectorstore.similarity_search(query)
-        return sub_docs[0].page_content
-
-    # query = "What can you tell me about the Celtics?"
-    
-    def get_llm_chain(self, output_parser):
-        QUERY_PROMPT = PromptTemplate(
-            input_variables=["question"],
-            template="""You are an AI language model assistant. Your task is to generate five 
-            different versions of the given user question to retrieve relevant documents from a vector 
-            database. By generating multiple perspectives on the user question, your goal is to help
-            the user overcome some of the limitations of the distance-based similarity search. 
-            Provide these alternative questions separated by newlines.
-            You must answer in Korean.
-            Original question: {question}""",
-        )
-        llm = ChatOpenAI(temperature=0)
-
-        # Chain
-        llm_chain = LLMChain(llm=llm, prompt=QUERY_PROMPT, output_parser=output_parser)
-        return llm_chain
-
-    def get_stuff_chain(self):
-        # Override prompts
-        document_prompt = PromptTemplate(
-            input_variables=["page_content"], template="{page_content}"
-        )
-        document_variable_name = "context"
-        llm = OpenAI()
-        stuff_prompt_override = """Given this text extracts:
-        -----
-        {context}
-        -----
-        Please answer the following question:
-        {query}"""
-        prompt = PromptTemplate(
-            template=stuff_prompt_override, input_variables=["context", "query"]
-        )
-        # Instantiate the chain
-        llm_chain = LLMChain(llm=llm, prompt=prompt)
-        
-        chain = StuffDocumentsChain(
-            llm_chain=llm_chain,
-            document_prompt=document_prompt,
-            document_variable_name=document_variable_name,
-        )
-        return chain
         
     def make_retriever(self, file_path):
         data = self.get_data(file_path)
@@ -263,11 +216,11 @@ class Search():
     
     def make_retriever_from_url(self, url_list):
         cached_embedder = self.get_cached_embedder()
+        docs = self.get_multi_data(url_list)
         # db = self.get_embeddings(docs, cached_embedder)
         # retriever = self.get_retriever(db)
         retriever = self.get_parent_document_retriever(cached_embedder)
-        docs = self.get_multi_data(url_list)
-        print(docs)
+        # print(docs)
         retriever.add_documents(docs, ids=None)
         pipeline_compression_retriever = self.get_pipeline_compression_retriever(retriever, cached_embedder)
         return pipeline_compression_retriever
@@ -279,25 +232,25 @@ class Search():
 
 if __name__ == "__main__":
     search = Search()
-    query = "고딩들이 농구하는 웹툰 알려줘"
+    query = "저승을 주제로 한 웹툰 알려줘"
     
-    file_name = "namu_new.csv"
-    file_path = os.path.join("..", "streamlit/src/model/data/webtoon", file_name)
+    file_name = "namu.csv"
+    file_path = "./namu.csv"
 
     absolute_file_path = os.path.abspath(file_path)
     # search.run(query, absolute_file_path)
     ## 화산귀환, 신의탑, 외모지상주의, 나이트런, 전지적 독자 시점, 재혼황후, 가비지타임, 내일, 삼국지톡, 놓지마 정신줄
     namu_list = [
-        # "https://namu.wiki/w/%ED%99%94%EC%82%B0%EA%B7%80%ED%99%98(%EC%9B%B9%ED%88%B0)",
+        "https://namu.wiki/w/%ED%99%94%EC%82%B0%EA%B7%80%ED%99%98(%EC%9B%B9%ED%88%B0)",
         "https://namu.wiki/w/%EC%8B%A0%EC%9D%98%20%ED%83%91",
-        # "https://namu.wiki/w/%EC%99%B8%EB%AA%A8%EC%A7%80%EC%83%81%EC%A3%BC%EC%9D%98(%EC%9B%B9%ED%88%B0)",
-        # "https://namu.wiki/w/%EB%82%98%EC%9D%B4%ED%8A%B8%EB%9F%B0",
+        "https://namu.wiki/w/%EC%99%B8%EB%AA%A8%EC%A7%80%EC%83%81%EC%A3%BC%EC%9D%98(%EC%9B%B9%ED%88%B0)",
+        "https://namu.wiki/w/%EB%82%98%EC%9D%B4%ED%8A%B8%EB%9F%B0",
         "https://namu.wiki/w/%EC%A0%84%EC%A7%80%EC%A0%81%20%EB%8F%85%EC%9E%90%20%EC%8B%9C%EC%A0%90(%EC%9B%B9%ED%88%B0)",
-        # "https://namu.wiki/w/%EC%9E%AC%ED%98%BC%20%ED%99%A9%ED%9B%84(%EC%9B%B9%ED%88%B0)",
+        "https://namu.wiki/w/%EC%9E%AC%ED%98%BC%20%ED%99%A9%ED%9B%84(%EC%9B%B9%ED%88%B0)",
         "https://namu.wiki/w/%EA%B0%80%EB%B9%84%EC%A7%80%ED%83%80%EC%9E%84",
-        # "https://namu.wiki/w/%EB%82%B4%EC%9D%BC(%EC%9B%B9%ED%88%B0)",
-        # "https://namu.wiki/w/%EC%82%BC%EA%B5%AD%EC%A7%80%ED%86%A1",
-        # "https://namu.wiki/w/%EB%86%93%EC%A7%80%EB%A7%88%20%EC%A0%95%EC%8B%A0%EC%A4%84"
+        "https://namu.wiki/w/%EB%82%B4%EC%9D%BC(%EC%9B%B9%ED%88%B0)",
+        "https://namu.wiki/w/%EC%82%BC%EA%B5%AD%EC%A7%80%ED%86%A1",
+        "https://namu.wiki/w/%EB%86%93%EC%A7%80%EB%A7%88%20%EC%A0%95%EC%8B%A0%EC%A4%84"
     ]
     now = time.time()
     pipeline_compression_retriever = search.make_retriever_from_url(namu_list)
