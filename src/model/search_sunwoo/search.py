db12bf0f77a0d3f7cb5c17e5a173b30cfd8885a6
sunwookim <sunkim09@g.skku.edu>
2024-01-29 15:12:56 +0900

    FEAT search finish

--- src/model/search_sunwoo/search.py
+++ src/model/search_sunwoo/search.py
@@ -14,13 +14,14 @@ from langchain_community.vectorstores import Chroma,  Qdrant, FAISS
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.document_loaders import DataFrameLoader
+from langchain_community.vectorstores.elasticsearch import ElasticsearchStore
 
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI, OpenAI
 
-from langchain.text_splitter import CharacterTextSplitter 
+from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
 
 from langchain.retrievers.document_compressors import EmbeddingsFilter
-from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers import ContextualCompressionRetriever, BM25Retriever, ParentDocumentRetriever
 from langchain.retrievers.document_compressors import DocumentCompressorPipeline
 
 import time
@@ -30,10 +31,12 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 import json
+import chromadb
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY_KSW")
+ELASTIC_API_KEY = os.getenv("ELASTIC_API_KEY")
 # OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 class Response(BaseModel):
     """Final response to the question being asked"""
@@ -52,10 +55,6 @@ class Search():
         """ Get data from csv file """
         loader = CSVLoader(
             file_path=file_path,
-            # csv_args={
-            # 'delimiter': ',',
-            # 'quotechar': '"',
-            # 'fieldnames': ['id', 'title', 'genre', 'description']},
             encoding = 'UTF-8'
         )
         data = loader.load()
@@ -67,54 +66,80 @@ class Search():
         data = loader.load()
         return data
     
-    def get_multi_data_from_dataframe(self, datas):
+    def get_data_from_dataframe(self, data):
+        """ Get multi data  """
+        # docs = []
+        # for data in datas:
+        #     loader = DataFrameLoader(data, page_content_column="text")
+        #     data = loader.lazy_load()
+        #     docs.append(data)
+        loader = DataFrameLoader(data, page_content_column="text")
+        data = loader.lazy_load()
+        return data
+    
+    def get_mulit_data_from_dataframe(self, datas):
         """ Get multi data  """
         docs = []
         for data in datas:
-            loader = DataFrameLoader(data, page_content_column="description")
-            for i in loader.lazy_load():
-                docs.append(i)
+            loader = DataFrameLoader(data, page_content_column="text")
+            data = loader.lazy_load()
+            docs.extend(data)
         return docs
         
     def get_text_splitter(self, docs):
         """ Split text """
-        text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=20)
+        text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=10)
         documents = text_splitter.split_documents(docs)
         return documents
     
     def get_cached_embedder(self):
         """ Get cached embedder -> Speed up """""
-        underlying_embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-
+        underlying_embeddings = OpenAIEmbeddings(
+            openai_api_key=OPENAI_API_KEY)
         store = LocalFileStore("./cache/")
-
         cached_embedder = CacheBackedEmbeddings.from_bytes_store(
             underlying_embeddings, store, namespace=underlying_embeddings.model
         )
         return cached_embedder
     
+    
     def get_embeddings(self, documents, cached_embedder, collection_name="webtoon"):
-        vectorstore = Chroma.from_documents(documents, cached_embedder,
+        vectorstore = Chroma.from_documents(
+            documents, 
+            cached_embedder,
             collection_name=collection_name)
         return vectorstore
     
-    def get_retriever(self, db):
+    def get_retriever(self, vectorstore):
         """ Create a retriever """
-        retriever = db.as_retriever(
-            search_type="similarity_score_threshold",
-            search_kwargs={
-                "score_threshold": 0.7,
-                "k": 1
-        }
+        retriever = vectorstore.as_retriever(
+            search_type="mmr",
         )
         return retriever
+    
+    def get_bm25_retriever(self, documents):
+        retriver = BM25Retriever.from_documents(documents)
+        return retriver
+        
+    def get_elastic_vector(self, cached_embedder, documents):
+        elasticsearch_url = "https://50da3596960c471fb7fa70548b0a71d1.us-central1.gcp.cloud.es.io:443"
+        elastic_vectorstore = ElasticsearchStore.from_documents(
+            documents,
+            cached_embedder,
+            es_url=elasticsearch_url,
+            es_api_key=ELASTIC_API_KEY,
+            index_name="webtoon",
+            strategy=ElasticsearchStore.ExactRetrievalStrategy()
+        )
+        elastic_vectorstore.client.indices.refresh(index="webtoon")
+        return elastic_vectorstore
     
     def get_pipeline_compression_retriever(self, retriever, embeddings):
         """ Create a pipeline of document transformers and a retriever """
         ## filters
-        splitter = CharacterTextSplitter(chunk_size=400, chunk_overlap=40)
+        splitter = CharacterTextSplitter(chunk_size=400, chunk_overlap=20)
         redundant_filter = EmbeddingsRedundantFilter(embeddings=embeddings)
-        relevant_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.8)
+        relevant_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.76)
         
         pipeline_compressor = DocumentCompressorPipeline(
             transformers=[splitter, redundant_filter, relevant_filter]
@@ -156,17 +181,16 @@ class Search():
     def get_agent(self, retriever_tool):
         system_message = """
         You are an AI responding to users searching for webtoons. 
-        Summarize the data in three lines or less and provide a friendly response in Korean.
+        Summarize the data three lines of less and answer by changing it to your kind speeking.
+        
+        title: {title}
         data: {data}
         
         You always follow these guidelines:
-            -If the answer isn't available within the context, state that fact
-            -Otherwise, answer to your best capability, refering to source of documents provided
-            -Only use examples if explicitly requested
-            -Do not introduce examples outside of the context
-            -Do not answer if context is absent
-            -Limit responses to three or four sentences for clarity and conciseness
+            -Limit responses to three lines for clarity and conciseness
             -You must answer in Koreans
+            -You must start with '찾으시는 작품은 {title} 입니다.'
+            -You must contains the summary of the webtoon
         """
         
         prompt = ChatPromptTemplate.from_messages(
@@ -187,6 +211,7 @@ class Search():
         
         agent = (
             {
+                "title": lambda x: x["title"],
                 "data": lambda x: x["data"],
                 "input": lambda x: x["input"],
                 "agent_scratchpad": lambda x: format_to_openai_function_messages(
@@ -214,21 +239,31 @@ class Search():
         #     # result.append(doc.page_content)
         #     result.append(doc)
         # return result
-        return docs[0].page_content
+        return docs
     
     def get_sub_relevant_documents(self, query, vectorstore):
         sub_docs = vectorstore.similarity_search(query)
         return sub_docs[0].metadata["title"]
     
     def make_retriever(self, datas):
-        docs = self.get_multi_data_from_dataframe(datas)
-        # print(docs)
+        cached_embedder = self.get_cached_embedder()
+        
+        docs = self.get_mulit_data_from_dataframe(datas)
         documents = self.get_text_splitter(docs)
         
-        cached_embedder = self.get_cached_embedder()
-        db = self.get_embeddings(documents, cached_embedder)
+        vectorstore = self.get_embeddings(documents, cached_embedder)
+        retriever = self.get_retriever(vectorstore)
         
-        retriever = self.get_retriever(db)
+        # elastic_vectorstore = self.get_elastic_vector(cached_embedder, documents)
+        # elastic_docs = elastic_vectorstore.similarity_search(
+        #     query,
+        #     k=5
+        # )
+        # print(elastic_docs)
+        # bm25_retriever = self.get_bm25_retriever(documents)
+        # bm25_docs = bm25_retriever.get_relevant_documents(query)
+        # print(bm25_docs)
+        
         pipeline_compression_retriever = self.get_pipeline_compression_retriever(retriever, cached_embedder)
         return pipeline_compression_retriever
     
@@ -236,26 +271,37 @@ class Search():
         retriever_tool = self.get_retriever_tool(pipeline_compression_retriever)
         result = self.get_all_relevant_documents(query, pipeline_compression_retriever)
         
-        agent_executor = self.get_agent(retriever_tool) 
-        result = agent_executor(
-            {   "data": result,
+        if len(result) == 0:
+            return "검색 결과가 없습니다."
+        agent_executor = self.get_agent(retriever_tool)
+        
+        response = agent_executor(
+            {   
+                "title": result[0].metadata["title"],
+                "data": result[0].page_content,
                 "input": query},
             return_only_outputs=True)
-        return result
+        return response
 
 if __name__ == "__main__":
     search = Search()
-    query = "재혼하는 웹툰 찾아줘"
-    
-    file_name = "namu_new.csv"
-    file_path = os.path.join("..", "streamlit/src/model/data/webtoon", file_name)
-
-    absolute_file_path = os.path.abspath(file_path)
-    df = pd.read_csv(absolute_file_path)
-    # print(df)
+    query = "일상물 추천해줘"
     datas = []
-    datas.append(df)
-    ## 화산귀환, 신의탑, 외모지상주의, 나이트런, 전지적 독자 시점, 재혼황후, 가비지타임, 내일, 삼국지톡, 놓지마 정신줄
+    # title_id = "703844"
+    
+    title_id_list = ['183559', '478261', '703844', '747269', '769209', '784248', '790713']
+    
+    file_name = "info.csv"
+    for title_id in title_id_list:
+        file_path = os.path.join("..", "streamlit/src/model/data/webtoon", title_id, file_name)
+        absolute_file_path = os.path.abspath(file_path)
+        
+        df = pd.read_csv(absolute_file_path)
+        # df = df.drop_duplicates()
+        # df.to_csv(absolute_file_path, index=False)
+        datas.append(df)
+    
+    ## 화산귀환, 신의탑, 전지적 독자 시점, 가비지타임, 선천적 얼간이들, 내가 키운 S급들, 대학원 탈출일지
     now = time.time()
     # pipeline_compression_retriever, vectorstore = search.make_retriever_from_url(namu_list)
     pipeline_compression_retriever = search.make_retriever(datas)
